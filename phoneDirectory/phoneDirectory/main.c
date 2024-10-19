b1d2216e3d127d17d6187fb7ceca3269112445c1
Artem <109523883+ArtemNikit1n@users.noreply.github.com>
2024-10-19 20:43:02 +0300

    Изменить пользовательский ввод

--- phoneDirectory/phoneDirectory/main.c
+++ phoneDirectory/phoneDirectory/main.c
@@ -5,7 +5,6 @@
 
 #include "workingWithAFile.h"
 
-#define _CRT_SECURE_NO_WARNINGS
 
 void outputOfReferenceInformation() {
     setlocale(LC_ALL, "Rus");
@@ -17,9 +16,10 @@ void outputOfReferenceInformation() {
     printf("\n5 - сохранить текущие данные в файл\n");
 }
 
-void userInput(short *commandNumber, bool *errorCode) {
+short userInput(bool *errorCode) {
     char *strCommandNumber = (char *)calloc(10, sizeof(char));
     char *endptrCommandNumber = NULL;
+    short commandNumber = -1;
 
     setlocale(LC_ALL, "Rus");
 
@@ -30,53 +30,58 @@ void userInput(short *commandNumber, bool *errorCode) {
     }
 
     scanf("%s", strCommandNumber);
-    *commandNumber = (int)strtol(strCommandNumber, &endptrCommandNumber, 10);
+    commandNumber = (int)strtol(strCommandNumber, &endptrCommandNumber, 10);
 
-    if (*commandNumber < 0 || *commandNumber > 6 || *endptrCommandNumber != '\0') {
-        *commandNumber = -1;
+    if (commandNumber < 0 || commandNumber > 6 || *endptrCommandNumber != '\0') {
         printf("Некорректный номер команды, попробуйте ещё раз\n");
         outputOfReferenceInformation();
+        free(strCommandNumber);
+        userInput(&errorCode);
+    } else {
+        free(strCommandNumber);
+        return commandNumber;
     }
-    free(strCommandNumber);
 }
 
-void callingTheFunction(short *commandNumber, bool* errorCode) {
+void callingTheFunction(short commandNumber, bool* errorCode) {
     struct NameAndPhoneNumber records = {
     .numberOfEntries = 0,
     .names = { { '\0' } },
     .phones = { { '\0' } }
     };
 
-    if (*commandNumber == 0) {
-        printf("0\n");
+    if (commandNumber == 0) {
+        printf("Выход из справочника...\n");
+        return;
     }
-    else if (*commandNumber == 1) {
-        printf("1\n");
+    else if (commandNumber == 1) {
+        addANewContact(&records);
     }
-    else if (*commandNumber == 2) {
+    else if (commandNumber == 2) {
         readingFromAFile(&records, "phoneDatabase.txt", &errorCode);
         printAllAvailableRecords(&records);
     }
-    else if (*commandNumber == 3) {
-        printf("3\n");
+    else if (commandNumber == 3) {
+        printf("Ещё не готово\n");
     }
-    else if (*commandNumber == 4) {
-        printf("4\n");
+    else if (commandNumber == 4) {
+        printf("Ещё не готово\n");
     }
-    else if (*commandNumber == 5) {
-        printf("5\n");
+    else if (commandNumber == 5) {
+        printf("Ещё не готово\n");
     }
 }
 
 int main(void) {
     bool errorCode = false;
-    short commandNumber = -1;
 
     outputOfReferenceInformation();
-    while ((commandNumber < 0 || commandNumber > 6) && errorCode == false) {
-        userInput(&commandNumber, &errorCode);
-    }
-    callingTheFunction(&commandNumber, &errorCode);
+    //while ((commandNumber < 0 || commandNumber > 6) && errorCode == false) {
+    //    userInput(&commandNumber, &errorCode);
+    //}
+    short commandNumber = userInput(&errorCode);
+
+    callingTheFunction(commandNumber, &errorCode);
 
     return errorCode;
 }
